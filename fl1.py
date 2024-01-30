b1905d1a872653affd161eac48c74db2f0deb43c
Mosnio <130111318+Mosnio@users.noreply.github.com>
2024-01-30 21:53:04 +0530

    Update fl1.py

--- fl1.py
+++ fl1.py
@@ -168,7 +168,7 @@ with requests.Session() as session:
                 print("Unexpected response format")
         else:
             print(f"Request failed with status code {response.status_code}")
-        if float(b)>=random.uniform(0.00005,0.00006):
+        if float(b)>=random.uniform(0.060020005,0.060070006):
             print('processing withdraw')
             wd_pay= f"csrf_token=&op=withdraw&type=faucetpay&amount=float(b)&withdraw_address=0x32464E5230fF20f249C8e0f571e4aA3337501b71&tfa_code="
             response = session.post(url, data=wd_pay,headers=login_headers)
